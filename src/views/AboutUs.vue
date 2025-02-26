4f0a419f7c79ad094cf2d8282d28601339860717
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2025-02-26 22:56:47 +0530

    fix css

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -3,8 +3,8 @@
       <v-card-title>About Us</v-card-title>
       <v-card-text>
         <p>Welcome to our application! We strive to provide the best user experience.</p>
-        <v-divider></v-divider>
-        <p>
+        <v-divider class="mt-2 mb-2"></v-divider>
+        <p class="mb-2">
           <strong>Our Mission:</strong> To make the world a better place through technology.
         </p>
         <p>
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -4,17 +4,17 @@
       <v-card-text>
         <p>Need assistance? Here are some common questions and support options:</p>
         <v-list>
-          <v-list-item>
+          <v-list-item class="">
             <v-list-item-icon>
               <v-icon>mdi-help-circle</v-icon>
             </v-list-item-icon>
-            <v-list-item-content>How to reset my password?</v-list-item-content>
+            <v-list-item-content> How to reset my password?</v-list-item-content>
           </v-list-item>
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-lifebuoy</v-icon>
             </v-list-item-icon>
-            <v-list-item-content>Contact customer support</v-list-item-content>
+            <v-list-item-content> Contact customer support</v-list-item-content>
           </v-list-item>
         </v-list>
       </v-card-text>
